05972f3f29e7cd96a33c084fa5f00d8d813ac45d
Victor Lira <victor.liracarlos@gmail.com>
2022-05-10 10:28:27 -0300

    Adicionado produtos base

--- assets/data/products.js
+++ assets/data/products.js
@@ -1,8 +1,69 @@
 const products = [
     {
         id: 1,
-        name: 30.00,
-        image: '',
+        name: 'Galaxy A13 Soft Cover',
+        category: 'Covers',
+        price: '$ 14,00',
+        color: [
+            '../img/Galaxy-A13-Soft-Black-Cover.png',
+            '../img/Galaxy-A13-Soft-Clear-Cover.png',
+        ]
+    },
+    {
+        id: 2,
+        name: 'Galaxy A13 Card Slot Cover',
+        category: 'Covers',
+        price: '$ 19,00',
+        color: [
+            '../img/Galaxy-A13-Card-Slot-Cover-Arctic-Blue.png',
+            '../img/Galaxy-A13-Card-Slot-Cover-Peach.png',
+        ]
+            
+    },
+    {
+        id: 3,
+        name: '20,000 mAh Battery Pack PD,',
+        category: 'PowerAccessories',
+        price: '$ 49,99',
+        color: [
+            '../img/mAh-Battery-Pack-PD-Dark-Gray.png',
+        ]
+            
+    },
+    {
+        id: 4,
+        name: 'Wireless Charger Trio,',
+        category: 'PowerAccessories',
+        price: '$ 89,90',
+        color: [
+            '../img/Wireless-Charger-Trio-Black.png',
+            '../img/Wireless-Charger-Trio-White.png',
+        ]
+            
+    },
+    {
+        id: 5,
+        name: 'Galaxy Buds Pro,',
+        category: 'Audio',
+        price: '$ 149,99',
+        color: [
+            '../img/Galaxy-Buds-Pro-Phantom-Black.png',
+            '../img/Galaxy-Buds-Pro-Phantom-Silver.png',
+            '../img/Galaxy-Buds-Pro-Phantom-Violet.png',
+        ]
+            
+    },
+    {
+        id: 6,
+        name: 'Galaxy Buds Pro,',
+        category: 'Audio',
+        price: '$ 149,99',
+        color: [
+            '../img/Galaxy-Buds-Pro-Phantom-Black.png',
+            '../img/Galaxy-Buds-Pro-Phantom-Silver.png',
+            '../img/Galaxy-Buds-Pro-Phantom-Violet.png',
+        ]
+            
     },
 ]
 
