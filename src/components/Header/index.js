e6e5fe39929cd205d0a03d5971e6675dd6f8c84b
Bianca Alves <biasantosalves04@gmail.com>
2022-05-16 21:20:01 -0300

    menu changes

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,61 +1,32 @@
 import React from 'react';
 import {View, Text, StyleSheet} from 'react-native';
-import { Ionicons} from '@expo/vector-icons';
-
+import { SimpleLineIcons} from '@expo/vector-icons';
 
 export default function header(){
     return(
-        
         <View style={styles.header}>
-            <Text style ={styles.text}> SAMSUNG </Text>
-            <View style={styles.icons}>
-                <Ionicons name="ios-search-outline" size={24} color="black" />
-                <Ionicons name="cart-outline" size={24} color="black" />
-                <View style={styles.menu}>
-                    <View style={styles.row}></View>
-                    <View style={styles.row}></View>
-                    <View style={styles.row}></View>
-                </View>
-            </View>
+            <Text style ={styles.text}> LOGO </Text>
+            <SimpleLineIcons name="menu" size={24} color="black" />
         </View>
-
     );
 }
+
 const styles = StyleSheet.create({
-    header:{
-        backgroundColor:'white',
-        marginTop:0,
-        width:'100vw',
-        padding:15,
-        alignItems:'center',
-        flexDirection:'row',
-        justifyContent:'space-between',
-        marginBottom:20
-
+    header: {
+        backgroundColor: 'white',
+        marginTop: 0,
+        width: '100vw',
+        padding: 15,
+        alignItems: 'center',
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        marginBottom: 20
     },
 
-    text:{
-        color:'black',
-        fontSize:20,
-        fontWeight:'bold'
-    },
-
-    icons:{
-        flexDirection:'row',
-        justifyContent:'space-between',
-        width:'105px',
-        marginRight:5
-    },
-
-    menu:{
-        marginTop:3
-    },
-
-    row:{
-        backgroundColor:'black',
-        height:'2px',
-        width:'20px',
-        margin:2.5
+    text: {
+        color: 'black',
+        fontSize: 20,
+        fontWeight: 'bold'
     },
 
 })
